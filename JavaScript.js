a813a4baa8663f6b8fef4c64254f6bd36ff1b405
Frank Huang <outcaster@gmail.com>
2012-09-10 17:30:42 -0500

    updated with new sample code

--- JavaScript.js
+++ JavaScript.js
@@ -33,6 +33,95 @@ Test.prototype.newFunc = function() {
 	alert("new prototype function");
 };
 
-// Should display an alet
+// Should display an alert
 MyClass.newFunc();
+
+/*
+Sample code demonstrating a template, a most recently used cache, a key, json implementation
+*/
+var mkv = (function($){
+	// private local variables
+	var me = {}, elementsStored = 0, cache = new Array(), mostRecentlyUsed = new Array(), sizeOfCache, templateUrl;
+	
+	// initializer
+	me.makeFetch = function(tmp, size) {
+		sizeOfCache = size;
+		elementsStored = 0;
+		templateUrl = tmp.slice(); // copy an array
+	}	
+	
+	me.fetch = function(key, callBack) {
+		
+		// form url where %s is replaced with key
+		var jsonurl = templateUrl.splice(templateUrl.indexOf("%s"), 2, key);
+		
+		// check cache
+		if(cache[key]!="undefined") {
+			
+			// remove from most recently used
+			for(i = 0; i < mostRecentlyUsed.length; i++) {
+				if(mostRecentlyUsed[i] == key) {
+					delete mostRecentlyUsed[i];
+					elementsStored--;
+					break;
+				}
+			}
+			
+			// add to most recently used
+			mostRecentlyUsed.push(key);
+			
+			// callback
+			return callBack(cache[key]);
+		}
+		
+		// cache miss, make a backend call
+		$.ajax({
+			url: jsonurl,
+			success : function(data) {
+				
+				// if cache is overflown
+				if(elementsStored > sizeOfCache) {
+					var tmp, lru;
+					
+					// get LRU key
+					tmp = mostRecentlyUsed[0];
+					
+					// delete from cache
+					delete cache[tmp];
+					
+					// remove least recently used
+					lru = mostRecentlyUsed.shift();
+					
+					// push new key as most recently used
+					mostRecentlyUsed.push(key);
+					
+					// incremente element stored
+					elementsStored++;
+					
+					// store in cache
+					cache[key] = data;
+					
+					// call callback
+					return callBack(cache[key]);
+					
+				} else {
+					// increate element stored
+					elementsStored++;
+					
+					// store in cache
+					cache[key] = data;
+					
+					// add to most recently used
+					mostRecentlyUsed.push(key);
+					
+					// call callback
+					return callBack(cache[key]);
+				}
+			}
+		});
+		
+	}
+
+	return me;
+}());
 
