648f47ec8c2d05262df2eb668848aafed090b4d8
Frank Huang <outcaster@gmail.com>
2012-09-10 13:36:21 -0500

    New Commit

---
+++ JavaScript.js
@@ -0,0 +1,21 @@
+// Self invoking anonymous function with namespace
+var namespace = (function() {
+
+	// private local variables
+	var my = {}, email = "feedback@test.com";
+	
+	my.sendFeedback = function() {
+		// has access to private variable email
+		alert(email);
+	}
+	
+	// return my, closure example
+	return my;
+
+})();
+
+// alerts the email
+namespace.sendFeedback();
+
+// undefined, no access to private variable
+alert(namespace.email);
